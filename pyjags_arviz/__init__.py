b691c9a0446b1dece0de7ab4c811d462f964d7e5
Michael Nowotny <nowotnym@gmail.com>
2020-05-17 11:04:29 -0700

    - added functionality for vector-valued parameters

--- pyjags_arviz/__init__.py
+++ pyjags_arviz/__init__.py
@@ -9,10 +9,18 @@ def convert_pyjags_samples_dict_to_arviz_inference_data(
     #         (parameter dimension, chain length, number of chains)
     # but arviz expects samples with shape
     #         (number of chains, chain length, parameter dimension)
-    reordered_samples \
-        = {name: np.swapaxes(sample, 0, 2)
-           for name, sample
-           in samples.items()}
+
+    parameter_name_to_samples_map = {}
+
+    for parameter_name, chains in samples.items():
+        parameter_dimension, chain_length, number_of_chains = chains.shape
+        if parameter_dimension == 1:
+            parameter_name_to_samples_map[parameter_name] = \
+                chains[0, :, :].transpose()
+        else:
+            for i in range(parameter_dimension):
+                parameter_name_to_samples_map[f'{parameter_name}_{i+1}'] = \
+                    chains[i, :, :].transpose()
 
     return az.InferenceData(
-        posterior=az.data.base.dict_to_dataset(reordered_samples))
+        posterior=az.data.base.dict_to_dataset(parameter_name_to_samples_map))
